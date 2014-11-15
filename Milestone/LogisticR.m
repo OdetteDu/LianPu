eb23d05c4014adc302f7dfb5e162ef09330af340
Neo Liu <sl3350@nyu.edu>
2014-11-15 09:25:22 -0800

    Change name

--- Milestone/LogisticR.m
+++ Milestone/LogisticR.m
@@ -1,25 +1,25 @@
 x_train=load('x_train.dat');
 y_train=load('y_train.dat');
 [num_train,num_feature]=size(x_train);
-for i=1:numTrain
-    if y(
+for i=1:num_train
+    if y_train(i)==-1
+        y_train(i)=0;
+    end
+end
 theta=zeros(num_feature,1);
 tolerance=0.01;
 change=100;
 prev=zeros(num_feature,1);
 count=0;
-while (change>(tolerance*num_feature))
+while (change>tolerance)
     count=count+1;
-    stepsize=0.01/sqrt(count);
+    stepsize=1/sqrt(count);
     prev=theta;
     for i=1:num_train
-        x_train(i,:)*theta
-        theta=theta+stepsize*(y_train(i)-(1/(1+exp((-1)*x_train(i,:)*theta))))*(x_train(i,:)');
+        theta=theta+stepsize*(y_train(i)-(1/(1+exp((-1)*x_train(i,:)*theta))))*(x_train(i,:).');
     end
     change=norm(theta-prev);
-    break;
 end
-theta;
 count
 correct=0;
 for i=1:num_train
@@ -30,7 +30,7 @@ for i=1:num_train
         end
     end
     if (score<0.5)
-        if (y_train(i)==-1) 
+        if (y_train(i)==0) 
             correct=correct+1;
         end
     end
@@ -42,6 +42,11 @@ y_test=load('y_test.dat');
 correct=0;
 [num_test,num_feature]=size(x_test);
 for i=1:num_test
+    if y_test(i)==-1
+        y_test(i)=0;
+    end
+end
+for i=1:num_test
     score=1/(1+exp((-1)*x_test(i,:)*theta));
     if (score>0.5)
         if (y_test(i)==1) 
@@ -49,7 +54,7 @@ for i=1:num_test
         end
     end
     if (score<0.5)
-        if (y_test(i)==-1) 
+        if (y_test(i)==0) 
             correct=correct+1;
         end
     end
